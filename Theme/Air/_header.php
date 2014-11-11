487464b88b014d0b92daad5c564a49e32b134ac0
votai <vothanhtai86@gmail.com>
2014-11-11 16:37:02 +0700

    add facebook like & g+

--- Theme/Air/_header.php
+++ Theme/Air/_header.php
@@ -2,10 +2,17 @@
 <html<?php echo ipHtmlAttributes(); ?>>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title"
+          content="NHÀ HÀNG HOA SƠN" />
+    <meta property="og:url"
+          content="http://www.nhahanghoason.com" />
+    <meta property="og:description"
+          content="Địa chỉ : Đường 12C, khu Quèn Ổi, xã Ninh Hòa, huyện Hoa Lư, tỉnh Ninh Bình." />
     <?php ipAddCss('assets/theme.css'); ?>
     <?php ipAddCss('assets/style.css'); ?>
     <?php echo ipHead(); ?>
     <script src="<?php echo ipThemeUrl("assets/jquery-1.11.0.min.js"); ?>" ></script>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
 </head>
 <body>
 <div class="wrapper clearfix">
@@ -25,10 +32,13 @@
                         <?php echo ipSlot('languages'); ?>
                     </div>
                 <?php } ?>
-                <div class="socials">
-                    <a href="http://facebook.com" target="_blank" class="fa fa-facebook"></a>
-                </div>
                 <div class="hotline">Hotline: 0986 711 048</div>
+                <div class="g_plus">
+                    <div class="g-plusone" data-href="http://www.nhahanghoason.com"></div>
+                </div>
+                <div class="facebook">
+                    <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.nhahanghoason.com&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>
+                </div>
             </div>
         </div>
     </header>
